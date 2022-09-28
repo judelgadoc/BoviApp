e0bc465a636e763cf30fddaa3d4b32e42c1c5032
judelgadoc <judelgadoc@unal.edu.co>
2022-09-28 00:47:50 -0500

    Signup. Pendiente: revisar colores y eso del gradient. Al ver la página desde un dispostivo móvil la navbar no funciona, pero de todas formas la navbar la puse de manera provisional para comprobar funcionalidad.
    signup.html.bk es un backup de la vista sin uso de template inheritance,
    signup.html también funciona pero usa la herencia. Pendiente preguntar
    cómo prefieren la cosa, html normalito o con herencia
    
    Se añadió un icono en la barra de título y se configura (no por
    completo) la carpeta static para luego enviar al servidor AWS más
    fácilmente. Pendiende preguntar cómo hizo Fredy para eso de resources
    porque no me quedó tan claro, y si es diferente a static sería un enredo

--- main/views.py
+++ main/views.py
@@ -1,3 +1,34 @@
 from django.shortcuts import render
+from django.http import HttpResponse, HttpResponseRedirect
 
-# Create your views here.
+from django.contrib.auth.hashers import make_password
+from django.contrib.auth.models import User
+from .models import *
+
+
+
+
+def index(request):
+    context = {'temp': 1}
+    return render(request, 'main/index.html', context)
+
+def login(request):
+    context = {'temp': 1}
+    return render(request, 'main/login.html', context)
+
+def signup(request):
+    if request.method == "POST":
+        user = User(username=request.POST['inputEmail'],
+                    first_name=request.POST['inputFirstName'],
+                    last_name=request.POST['inputLastName'],
+                    email=request.POST['inputEmail'],
+                    password=make_password(request.POST['inputPassword'])
+        )
+        user.save()
+        usuario = Usuario(user=user,
+                          tipo=TipoUsuario.objects.get(pk=int(request.POST['inputType'])),
+                          direccion=request.POST['inputAddress'],
+                          telefono=request.POST['inputPhone'])
+        usuario.save()
+        return HttpResponseRedirect('/main/') ## Aquí va el index
+    return render(request, 'main/signup.html', {'temp': 1})
