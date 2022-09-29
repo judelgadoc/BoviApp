c9a0a38d50c578a03c479527528eeebe7d014690
frgonzalezP <frgonzalez@unal.edu.co>
2022-09-29 01:26:57 -0500

    Aplicacion de las vistas Index Y Login
    Adaptabilidad preliminar de Index
    Login Responsivo, No Estilo aun

--- BoviApp/urls.py
+++ BoviApp/urls.py
@@ -14,8 +14,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path
+from main import views
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('',views.index),
+    path('login', views.login, name='login'),
 ]
--- main/views.py
+++ main/views.py
@@ -1,3 +1,30 @@
+from django.contrib import messages
 from django.shortcuts import render
+from django.http import HttpResponse, HttpResponseRedirect
+from django.contrib.auth.hashers import make_password
+from django.contrib.auth.models import User
+from .models import *
 
 # Create your views here.
+
+def index(request):
+    context = {'temp': 1}
+    return render(request, 'main/index.html', context)
+
+   
+def login(request):
+    try:
+        del request.session['email_f']
+    except:
+        print("No hay seccion iniciada")   
+    if request.method=='POST':
+       email_f=request.POST['correo']
+       contraseña=(request.POST['contra'])
+       print(contraseña)
+       veri=User.objects.filter(username=email_f,password=contraseña).exists()
+       if veri ==True:
+        request.session['email_f']=email_f
+        return render(request,'main/index.html',{'email_f':email_f})
+       else:
+        messages.add_message(request, level=messages.WARNING,message='Nombre de usuario o Contraseña Incorrectos')
+    return render(request,'main/login.html')
